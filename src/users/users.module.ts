2b7decd049c4693be3958a36499faa8b0a7363f3
vinoles <felipe.vinoles@gmail.com>
2026-07-06 20:31:36 +0200

    fix(auth): harden refresh token rotation

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,11 +3,12 @@ import { AuthModule } from '../auth/auth.module';
 import { PrismaModule } from '../prisma/prisma.module';
 import { RolesGuard } from '../auth/roles.guard';
 import { SelfOrAdminGuard } from '../auth/self-or-admin.guard';
+import { UsersLookupModule } from './users-lookup.module';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
-  imports: [PrismaModule, AuthModule],
+  imports: [PrismaModule, UsersLookupModule, AuthModule],
   controllers: [UsersController],
   providers: [UsersService, RolesGuard, SelfOrAdminGuard],
   exports: [UsersService],
